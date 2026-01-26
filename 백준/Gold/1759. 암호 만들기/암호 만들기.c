bbe2056395c9b32644ea47cd24700f2e94c58397
svxcza-oss <svxcza@gmail.com>
2026-01-26 23:58:47 +0900

    Update 암호 만들기.c

--- 백준/Gold/1759. 암호 만들기/암호 만들기.c
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.c
@@ -14,13 +14,13 @@ int isT(char c){//자음인지 판별하는 함수
 void init_line(int m){//문자 정렬
     int cnt = 0;
     
-    for(int i = 0; i<m; i++){
+    for(int i = 0; i<m; i++){//문자를 입력받아 배열에 알맞는 자리수 +1
         char temp;
         scanf(" %c", &temp);
         input_line[temp - 'a'] = 1;
     }
     
-    for(int i = 0; i<26; i++){
+    for(int i = 0; i<26; i++){ //완성된 배열결과를 이용하여 정렬.
         if(input_line[i] == 1){
             sorted[cnt] = i + 'a';
             cnt++;
@@ -52,6 +52,10 @@ void dfsA(int depth, int index){//dfs를 이용해 조합 탐색
         dfsA(depth + 1, i + 1);
     }
 }
+/*
+  선형배열을 트리처럼 생각해서 원소를 하나씩 고정시키고 조건에 맞게 탐색해 조합.
+  모든 경우의 조합을 찾아보고 조건에 맞는 결과만 출력함.
+  */
 
 
 int main(void){
@@ -63,4 +67,5 @@ int main(void){
     return 0;
     
 }
+
 
