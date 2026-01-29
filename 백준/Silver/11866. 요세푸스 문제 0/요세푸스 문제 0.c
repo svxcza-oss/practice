a10e870b8f462a322e178f01fa7ff237c1ab2b1e
svxcza-oss <svxcza@gmail.com>
2026-01-29 18:22:17 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 4904 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define MAX 1000001
+//11866
+
+int main(void){
+    int n,m;
+    scanf("%d %d", &n,&m);
+    
+    int rear=0; int front=0; int flag = 0; int arr[MAX];
+    
+    for(int i = 1; i<=n; i++){
+        arr[rear++] = i;
+    }
+    
+    printf("<");
+    while(rear > front){
+        for(int i = 0; i<m-1; i++){
+            int flag = arr[front++]; //arr[1]값을 flag에 넣음 --> flag = 1
+            arr[rear++] = flag; //arr[맨끝] = 1.
+            /*계속 반복하면(예제 입력 7 3)
+             2 3 4 5 6 7 1
+             3 4 5 6 7 1 2
+             m-1번 -> 2번 반복
+             */
+        }
+        printf("%d", arr[front++]); //반복이 끝났음으로 3 출력.
+        //그리고 front는 +1이 되므로 논리적으로 3은 없어짐.
+        //4부터 다시 반복.
+        if(front < rear) printf(", ");
+    }
+    printf(">");
+    
+}
+
+
+
