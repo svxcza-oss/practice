8d9929fd11abada0250bbba08a112a277bb19c2f
svxcza-oss <svxcza@gmail.com>
2026-02-19 00:18:24 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int n,m;
+int vistd[10];
+int arr[10];
+int result[10];
+
+void dfsA(int dpt){
+    if (dpt == m) {
+    for(int i = 0; i<m; i++) printf("%d ", result[i]);
+    
+    printf("\n");
+    return;
+    }
+    
+    //n과 m(5)코드에서 중복제거만 하면 됨.
+    int prev_num = -1; //추가코드
+    for(int i = 0; i<n; i++){ //깊이 탐색 재귀호출
+        if(!vistd[i] && prev_num != arr[i]){ //추가코드
+            vistd[i] = 1;
+            result[dpt] = arr[i]; //n과 m(1) 변형
+            
+            prev_num = arr[i];//추가코드
+            dfsA(dpt+1);
+            
+            vistd[i] = 0;
+        }
+    }
+    
+}
+
+int main(void){
+    scanf("%d %d", &n,&m);
+    for(int i = 0; i<n; i++) scanf("%d", &arr[i]);
+    
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n-1-i; j++){
+            if(arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    dfsA(0);
+    return 0;
+}
+
