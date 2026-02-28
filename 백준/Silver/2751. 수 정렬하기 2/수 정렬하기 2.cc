4e960142cfb2f469b35bc900be6b87ca557c9b9e
svxcza-oss <svxcza@gmail.com>
2026-02-28 18:36:51 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 248 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int n; cin >> n;
+    vector<int> arr(n);
+
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+    
+    sort(arr.begin(), arr.end());
+    
+    for(int i = 0; i<n; i++) cout << arr[i] << "\n";
+    return 0;
+}
